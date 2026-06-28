23ab2d2169fccd09726be319f59a5057a2706afa
Harman <harman.preet.singh81626.cse@gamil.com>
2026-06-28 12:56:05 +0530

    Ast

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -1,7 +1,7 @@
 package com.craftinginterpreters.tool;
 
 import java.io.IOException;
-import java.io.PrinterWriter;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,26 +23,46 @@ public class GenerateAst {
     private static void defineAst(
             String outputDir, String baseName, List<String> types) throws IOException {
         String path = outputDir + "/" + baseName + ".java";
-        PrinterWriter writer = new PrinterWriter(path, "UTF-8");
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
 
         writer.println("package com.craftinginterpreters.lox;");
         writer.println();
         writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
         // The Ast classes
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        // The base accept() method.
+        writer.println();
+        writer.println("   abstract <R> R accept(Visitor<R> visitor);");
         writer.println("}");
+
         writer.close();
 
     }
 
+    private static void defineVisitor(
+            PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println("    }");
+    }
+
     private static void defineType(
-            PrinterWriter writer, String baseName,
+            PrintWriter writer, String baseName,
             String className, String fieldList) {
         writer.println("  static class " + className + " extends " + baseName + " {");
 
@@ -58,6 +78,14 @@ public class GenerateAst {
             writer.println("    this." + name + " = " + name + ";");
         }
 
+        writer.println("    }");
+
+        // Visitor pattern.
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("     return visitor.visit" +
+                className + baseName + "(this);");
         writer.println("    }");
 
         // Fields.
